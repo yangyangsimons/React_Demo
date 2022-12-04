a1a493a3b477afcec8c11f3df00c3e6dbc2cb4e4
shuati <ouyangyly@gmail.com>
2022-12-04 08:22:12 +0000

    adjust the code

---
+++ interactive-meme-generator/src/App.js
@@ -0,0 +1,7 @@
+import React from "react"
+import Header from "./component/Header"
+export default function(){
+  return (
+    <Header />
+  )
+}
---
+++ interactive-meme-generator/src/component/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <header>
+            <img src=""/>
+            <p>Meme Generator</p>
+            <p>React Course - project 3</p>
+        </header>
+    )
+}
