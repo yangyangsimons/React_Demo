05cbdddd6bd951095e61a1a180af5fdc2233724d
shuati <ouyangyly@gmail.com>
2023-02-17 17:09:22 +0000

    interactive image changing

--- interactive-meme-generator/src/App.js
+++ interactive-meme-generator/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "./component/Header"
 import Meme from "./component/Meme"
 
+
 export default function(){
   return (
     <div>
--- interactive-meme-generator/src/component/Meme.js
+++ interactive-meme-generator/src/component/Meme.js
@@ -1,13 +1,41 @@
 import React from "react"
+import memesData from "../memesData"
+
+    /**
+     * Challenge: Save the random meme URL in state
+     * - Create new state called `memeImage` with an
+     *   empty string as default
+     * - When the getMemeImage function is called, update
+     *   the `memeImage` state to be the random chosen
+     *   image URL
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
+     */
 
 export default function Meme(){
+    const [memeImage, setMemeImage] = React.useState('')
+
+    function getNewImage(e){
+        e.preventDefault()
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMemeImage(preMemeImage => {
+            preMemeImage = url
+            return preMemeImage
+        })
+
+    } 
     return (
         <main>
             <form className="form">
                 <input type="text" className="form--inputs"/>
                 <input type="text" className="form--inputs"/>
-                <button className="form--button">Get a new meme image  🖼</button>
+                <button className="form--button" onClick={getNewImage}>Get a new meme image  🖼</button>
             </form>
+            <div className="">
+                <img src={memeImage}/>
+            </div>
         </main>
 
     ) 
