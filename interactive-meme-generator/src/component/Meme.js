3a26e555bea3291c9c89b3dd0415318b542c004b
shuati <ouyangyly@gmail.com>
2023-02-19 17:09:22 +0000

    a simple sign up web page

--- interactive-meme-generator/src/component/Meme.js
+++ interactive-meme-generator/src/component/Meme.js
@@ -2,27 +2,41 @@ import React from "react"
 import memesData from "../memesData"
 
     /**
-     * Challenge: Save the random meme URL in state
-     * - Create new state called `memeImage` with an
-     *   empty string as default
-     * - When the getMemeImage function is called, update
-     *   the `memeImage` state to be the random chosen
-     *   image URL
-     * - Below the div.form, add an <img /> and set the
-     *   src to the new `memeImage` state you created
+     * Challenge: Update our state to save the meme-related
+     * data as an object called `meme`. It should have the
+     * following 3 properties:
+     * topText, bottomText, randomImage.
+     * 
+     * The 2 text states can default to empty strings for now,
+     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+     * 
+     * Next, create a new state variable called `allMemeImages`
+     * which will default to `memesData`, which we imported above
+     * 
+     * Lastly, update the `getMemeImage` function and the markup 
+     * to reflect our newly reformed state object and array in the
+     * correct way.
      */
 
 export default function Meme(){
-    const [memeImage, setMemeImage] = React.useState('')
+    const meme = {
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    }
+    const [memeState, setMemeState] = React.useState(meme)
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getNewImage(e){
         e.preventDefault()
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         const url = memesArray[randomNumber].url
-        setMemeImage(preMemeImage => {
-            preMemeImage = url
-            return preMemeImage
+        setMemeState(preState => {
+            return {
+                ...preState,
+                randomImage: url
+            }
         })
 
     } 
@@ -34,7 +48,7 @@ export default function Meme(){
                 <button className="form--button" onClick={getNewImage}>Get a new meme image  🖼</button>
             </form>
             <div className="">
-                <img src={memeImage}/>
+                <img src={memeState.randomImage}/>
             </div>
         </main>
 
